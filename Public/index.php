ed6e05f87d12e36a8c401e3bb9da432923aa7e23
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-03-09 14:45:13 +0100

    first commit

---
+++ Public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// use Router\Router;
+
+require './../vendor/autoload.php';
+
+// $router = new Router();
+
+// var_dump($router);
+
+// $router->register('/', function () {
+//   return 'HomePage';
+// });
+
+// $router->register('/contact', function () {
+//   return 'ContactPage';
+// });
+
+// echo $router->resolve($_SERVER['REQUEST_URI']);
+
+echo 'sdfsdfsdf';
